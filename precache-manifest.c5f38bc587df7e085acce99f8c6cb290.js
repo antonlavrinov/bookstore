dcc0c889540290e3762f2aba0981d9c1eec28fec
antonlavrinov <alv.anton.lavrinov@gmail.com>
2020-03-29 14:50:42 +0300

    Updates

---
+++ precache-manifest.c5f38bc587df7e085acce99f8c6cb290.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a5518704fb06f10631d6d832ed54b9",
+    "url": "/bookstore/index.html"
+  },
+  {
+    "revision": "6c7c530930caf9e71fa1",
+    "url": "/bookstore/static/css/main.1bc60074.chunk.css"
+  },
+  {
+    "revision": "2c78b408379215899fe9",
+    "url": "/bookstore/static/js/2.941a02b4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bookstore/static/js/2.941a02b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7c530930caf9e71fa1",
+    "url": "/bookstore/static/js/main.01f20acd.chunk.js"
+  },
+  {
+    "revision": "085fa06d3b1b7ada148a",
+    "url": "/bookstore/static/js/runtime-main.d475adb8.js"
+  },
+  {
+    "revision": "1250cdf8469c717febbf80f220f07558",
+    "url": "/bookstore/static/media/FontAwesome5Free-Regular.1250cdf8.svg"
+  },
+  {
+    "revision": "17a79efa7af839b62c10116548386f47",
+    "url": "/bookstore/static/media/FontAwesome5Free-Regular.17a79efa.ttf"
+  },
+  {
+    "revision": "45a5981b9f908e96854c601e42da809f",
+    "url": "/bookstore/static/media/FontAwesome5Free-Regular.45a5981b.woff2"
+  },
+  {
+    "revision": "66b51f401a4bacc7d6134e9617936f16",
+    "url": "/bookstore/static/media/FontAwesome5Free-Regular.66b51f40.eot"
+  },
+  {
+    "revision": "e43f57f28b58ba1c16d7429164222f03",
+    "url": "/bookstore/static/media/FontAwesome5Free-Regular.e43f57f2.woff"
+  },
+  {
+    "revision": "4460a9ad8e621d55d2ba0373b296fdec",
+    "url": "/bookstore/static/media/GoogleSans-Bold.4460a9ad.ttf"
+  },
+  {
+    "revision": "7350afb5bd4df52b4cf0ff16931b7e07",
+    "url": "/bookstore/static/media/GoogleSans-Bold.7350afb5.woff"
+  },
+  {
+    "revision": "8e05b4808da2be006b59ad8bd7058e86",
+    "url": "/bookstore/static/media/GoogleSans-Bold.8e05b480.woff2"
+  },
+  {
+    "revision": "b9844cb42af9037a6cb7c7d4ac5145e6",
+    "url": "/bookstore/static/media/GoogleSans-Bold.b9844cb4.eot"
+  },
+  {
+    "revision": "c8e556fa9a984c38209d7285f9bba841",
+    "url": "/bookstore/static/media/GoogleSans-Bold.c8e556fa.svg"
+  },
+  {
+    "revision": "36ca4a0a9acdbd40792e4b7fa24944e6",
+    "url": "/bookstore/static/media/GoogleSans-Regular.36ca4a0a.svg"
+  },
+  {
+    "revision": "603fdc362270fc0862e02ef9e99b4358",
+    "url": "/bookstore/static/media/GoogleSans-Regular.603fdc36.ttf"
+  },
+  {
+    "revision": "a042b2dc2a34217aba92815e501a2071",
+    "url": "/bookstore/static/media/GoogleSans-Regular.a042b2dc.woff"
+  },
+  {
+    "revision": "a71ab91a87a49294de19e927e0b39bf1",
+    "url": "/bookstore/static/media/GoogleSans-Regular.a71ab91a.woff2"
+  },
+  {
+    "revision": "cb5312f51d208e0fd53fae20839335ab",
+    "url": "/bookstore/static/media/GoogleSans-Regular.cb5312f5.eot"
+  },
+  {
+    "revision": "07a8a8ce3f4f7e714350c2529f070578",
+    "url": "/bookstore/static/media/MuseoSansCyrl-500.07a8a8ce.woff"
+  },
+  {
+    "revision": "1d7a7e5d778082a4c570f5cbcc21f6b8",
+    "url": "/bookstore/static/media/MuseoSansCyrl-500.1d7a7e5d.ttf"
+  },
+  {
+    "revision": "7c330a9e60b1e0e1c468d65cad0904c5",
+    "url": "/bookstore/static/media/MuseoSansCyrl-500.7c330a9e.eot"
+  },
+  {
+    "revision": "b533077ca00019bab65f5538f75d54e3",
+    "url": "/bookstore/static/media/MuseoSansCyrl-500.b533077c.woff2"
+  },
+  {
+    "revision": "fe8c54f3ee6921e4d46f38016c26406e",
+    "url": "/bookstore/static/media/MuseoSansCyrl-500.fe8c54f3.svg"
+  },
+  {
+    "revision": "0d2778e51e782e8533176bca4be109f2",
+    "url": "/bookstore/static/media/MuseoSansCyrl-700.0d2778e5.ttf"
+  },
+  {
+    "revision": "2b59c9837f2473baf0f18e4b21c7d424",
+    "url": "/bookstore/static/media/MuseoSansCyrl-700.2b59c983.woff2"
+  },
+  {
+    "revision": "d26da7a2e6abe41eae78c496cc1329c9",
+    "url": "/bookstore/static/media/MuseoSansCyrl-700.d26da7a2.woff"
+  },
+  {
+    "revision": "ed58a6a81e2590782dc20d942a4b4427",
+    "url": "/bookstore/static/media/MuseoSansCyrl-700.ed58a6a8.eot"
+  },
+  {
+    "revision": "fe82597d4477af60ebd390d71fcbd4ec",
+    "url": "/bookstore/static/media/MuseoSansCyrl-700.fe82597d.svg"
+  },
+  {
+    "revision": "7904b702a47dcaa2037e8f551a88a4af",
+    "url": "/bookstore/static/media/book_offer3.7904b702.png"
+  },
+  {
+    "revision": "ea9415cfac0a6c7a3bfc0849d7003b09",
+    "url": "/bookstore/static/media/no_cov.ea9415cf.jpeg"
+  }
+]);
